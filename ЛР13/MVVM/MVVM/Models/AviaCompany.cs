5d43cdd6921d0443e9355352b3560e88cd10e907
Eugene Kurlovich <zhenikpggkurlovich@gmail.com>
2017-06-03 19:52:41 +0300

    LR13

---
+++ ЛР13/MVVM/MVVM/Models/AviaCompany.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MVVM.Models
+{
+   public class AviaCompany : INotifyPropertyChanged
+    {
+
+        public void OnPropertyChanged(string propertyName)
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+
+        public int number { get; set; }
+        public string weekDay { get; set; }
+        public string way { get; set; }
+        public int ammount { get; set; }
+
+        public AviaCompany(int n, string wd, string w, int a)
+        {
+            number = n;
+            weekDay = wd;
+            way = w;
+            ammount = a;
+        }
+
+        public AviaCompany()
+        {
+
+        }
+    }
+}
